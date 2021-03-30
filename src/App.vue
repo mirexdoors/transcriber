3b2c2f01219eff4064ea806c04a5752542049e0c
mirexdoors <mirex.doors@mail.ru>
2021-03-31 01:34:46 +0700

    add proto layout

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,193 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+	<v-app>
+		<v-app-bar
+			app
+			light
+			class="justify-center accent"
+		>
+			<v-container fluid>
+				<h1 class="title text-center d-flex justify-center text--primary">Transcriber</h1>
+			</v-container>
+		</v-app-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+		<v-main>
+			<v-container>
+				<v-row>
+					<v-col
+						cols="6"
+						class="secondary white--text"
 
-      <v-spacer></v-spacer>
+                    >
+                        <v-card
+                            outlined
+                            >
+                        <v-file-input
+                            chips
+                            multiple
+                            label="File input w/ chips"
+                        />
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+                        <div
+                        style="height: 600px;"
+                        >Здесь результат распознавания текста</div>
+                        </v-card>
+                    </v-col>
+					<v-col
+						cols="6"
+						class="secondary white--text">
+                        <v-card
+                            outlined
+                        >
+                        Здесь параметры
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+                        <v-select
+                            :items="[1,2,3]"
+                            label="Timing"
+                            outlined
+                        />
+
+
+                        <v-switch
+                            inset
+                            label="Param 1"
+                        />
+
+                        </v-card>
+                    </v-col>
+				</v-row>
+
+				<v-row>
+					<v-col cols="12" class="secondary">
+                        <v-card-title>
+                            Archive
+                            <v-spacer></v-spacer>
+                            <v-text-field
+                                v-model="search"
+                                append-icon="mdi-magnify"
+                                label="Search"
+                                single-line
+                                hide-details
+                            ></v-text-field>
+                        </v-card-title>
+						<v-data-table
+							:headers="headers"
+							:items="desserts"
+							:items-per-page="5"
+							class="elevation-1"
+						>
+						</v-data-table>
+
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
+  export default {
+    name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    components: {},
 
-  data: () => ({
-    //
-  }),
-};
+    data: () => ({
+        search: '',
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        {text: 'Calories', value: 'calories'},
+        {text: 'Fat (g)', value: 'fat'},
+        {text: 'Carbs (g)', value: 'carbs'},
+        {text: 'Protein (g)', value: 'protein'},
+        {text: 'Iron (%)', value: 'iron'},
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        },
+      ],
+    }),
+  };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#343434',
+        secondary: '#AEAEAE',
+        accent: '#FFFFFF',
+      }
+    }
+  }
 });
