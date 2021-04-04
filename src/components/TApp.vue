c8139250e5ccdb41a1a8bfeb95fdb53b1e68cb9f
mirexdoors <mirex.doors@mail.ru>
2021-04-05 01:38:24 +0700

    add video -> wav convert in upload

--- src/components/TApp.vue
+++ src/components/TApp.vue
@@ -1,113 +1,112 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col
-          cols="5"
-          class="secondary white--text pt-8"
+	<v-container>
+		<v-row>
+			<v-col
+				cols="5"
+				class="secondary white--text pt-8"
 
-      >
-        <v-card class="pa-4">
-          <t-file-input class="mb-8"/>
+			>
+				<v-card class="pa-4">
+					<t-file-input class="mb-8"/>
 
-          <div class="caption mb-2">Parameters:</div>
+					<div class="caption mb-2">Parameters:</div>
 
-          <v-select
-              :items="[15,30,60, 120]"
-              :value="15"
-              label="Timing (sec)"
-              outlined
-          />
+					<v-select
+						:items="[15,30,60, 120]"
+						:value="15"
+						label="Timing (sec)"
+						outlined
+					/>
 
-          <v-switch
-              color="green darken-4"
-              inset
-              v-model="switchValue"
-              label="Включить расстановку пунктуации"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              v-model="switchValue"
-              label="Разделение по спикерам"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              label="Включить расстановку тэгов"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              label="Определить эмоции"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              disabled
-              label="Выделить ключевые места"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              disabled
-              label="Убрать ненормативную лексику"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              disabled
-              label="Убрать слова-паразиты"
-          />
+					<v-switch
+						color="green darken-4"
+						inset
+						v-model="switchValue"
+						label="Включить расстановку пунктуации"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						v-model="switchValue1"
+						label="Разделение по спикерам"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						label="Включить расстановку тэгов"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						label="Определить эмоции"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						disabled
+						label="Выделить ключевые места"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						disabled
+						label="Убрать ненормативную лексику"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						disabled
+						label="Убрать слова-паразиты"
+					/>
 
-          <div class="caption MB-2">Text domains</div>
-          <v-switch
-              color="green darken-4"
-              inset
-              v-model="switchValue"
-              disabled
-              label="Пользовательский"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              disabled
-              label="Телекомуникационный"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              disabled
-              label="Военный"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              dense
-              disabled
-              label="Юридический"
-          />
-          <v-switch
-              color="green darken-4"
-              inset
-              dense
-              disabled
-              label="Нефтегаовый"
-          />
-        </v-card>
-      </v-col>
+					<div class="caption MB-2">Text domains</div>
+					<v-switch
+						color="green darken-4"
+						inset
+						v-model="switchValue2"
+						disabled
+						label="Пользовательский"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						disabled
+						label="Телекомуникационный"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						disabled
+						label="Военный"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						dense
+						disabled
+						label="Юридический"
+					/>
+					<v-switch
+						color="green darken-4"
+						inset
+						dense
+						disabled
+						label="Нефтегаовый"
+					/>
+				</v-card>
+			</v-col>
 
-      <v-col
-          cols="7"
-          class="secondary white--text pt-8"
-      >
-        <v-card class="pa-4">
-          <div class="caption mb-2">Output as text:</div>
-          <v-textarea
-              name="input-7-1"
-              filled
-              readonly
-              height="484"
-              value="The The Woodman set to work at once, and so sharp was his axe that the tree was soon c
+			<v-col
+				cols="7"
+				class="secondary white--text pt-8"
+			>
+				<v-card class="pa-4">
+					<div class="caption mb-2">Output as text:</div>
+					<v-textarea
+						name="text"
+						filled
+						readonly
+						value="The The Woodman set to work at once, and so sharp was his axe that the tree was soon c
                   hopped nearly through The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly throughThe Woodman set to work at once, and so sharp was his axe that the tree was soon c
                   hopped nearly through The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly throughThe Woodman set to work at once, and so sharp was his axe that the tree was soon c
                   hopped nearly through The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly throughThe Woodman set to work at once, and so sharp was his axe that the tree was soon c
@@ -122,57 +121,62 @@
                   hopped nearly through The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly throughWoodman set to work at once, and so sharp was his axe that the tree was soon c
                   hopped nearly through The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly throughThe Woodman set to work at once, and so sharp was his axe that the tree was soon c
                   hopped nearly through The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through"
-          />
+						class="textarea"
+					/>
 
-          <div class="caption">Download output as file:</div>
-          <v-chip-group>
-            <v-chip
-                v-for="tag in tags"
-                :key="tag"
-            >
-              {{ tag }}
-            </v-chip>
-          </v-chip-group>
+					<div class="caption">Download output as file:</div>
+					<v-chip-group>
+						<v-chip
+							v-for="extension in extensions"
+							:key="extension"
+						>
+							{{ extension }}
+						</v-chip>
+					</v-chip-group>
 
-        </v-card>
-      </v-col>
-    </v-row>
+				</v-card>
+			</v-col>
+		</v-row>
 
-    <v-row>
-      <v-col
-          cols="12"
-          class="secondary"
-      >
-        <t-archive></t-archive>
-      </v-col>
-    </v-row>
-  </v-container>
+		<v-row>
+			<v-col
+				cols="12"
+				class="secondary"
+			>
+				<t-archive></t-archive>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-import TFileInput from "@/components/TFileInput";
-import TArchive from "@/components/TArchive";
+  import TFileInput from "@/components/TFileInput";
+  import TArchive from "@/components/TArchive";
 
-export default {
-  name: "TApp",
-  components: {
-    TFileInput,
-    TArchive,
-  },
-  data() {
-    return {
-      switchValue: true,
-      tags: [
-        '.DOCX',
-        '.PDF',
-        '.TXT',
-        '.HTML',
-      ],
-    }
-  },
-}
+  export default {
+    name: "TApp",
+    components: {
+      TFileInput,
+      TArchive,
+    },
+    data() {
+      return {
+        switchValue: true,
+        switchValue1: true,
+        switchValue2: true,
+        extensions: [
+          '.DOCX',
+          '.PDF',
+          '.TXT',
+          '.HTML',
+        ],
+      }
+    },
+  }
 </script>
 
-<style scoped>
-
+<style>
+	.textarea textarea {
+		min-height: 400px !important;
+	}
 </style>
--- src/components/TFileInput.vue
+++ src/components/TFileInput.vue
@@ -1,13 +1,14 @@
 <template>
 	<div id="file-drag-drop">
 		<form
-			v-if="!file"
+			v-show="!file"
 			ref="fileForm"
+			:style="!file ? 'height: 200px;' : ''"
 			:class="isDragover ? 'hover secondary' : ''"
-			class="form bordered d-flex flex-column align-center justify-center"
+			class="form bordered flex-column align-center justify-center"
 		>
 
-			<span class="caption text--secondary">Drop the files here</span>
+			<span class="caption text--secondary">Drop the files (.wav, .mp3, .mov, .mp4) here</span>
 			<div class="caption my-2">or</div>
 			<v-btn
 				elevation="2"
@@ -25,53 +26,67 @@
 		</form>
 
 		<div
-			v-else
-			class="form d-flex flex-column  align-center justify-center"
+			v-show="file"
+			:style="file ? 'height: 200px;' : ''"
+			class="form d-flex  align-center justify-center"
 		>
-			<v-chip
-				close
-				class="mb-8"
-				@click:close="removeFile()"
+			<div
+				v-if="!isPreload"
+				class="d-flex flex-column  align-center justify-center"
+				style="width: 100%;"
 			>
-				{{ file.name }}
-				({{ fileSize }})
-			</v-chip>
-
-			<v-btn
-				class="submit-button primary"
-				v-show="file"
-			>
-				Submit
-				<v-icon
-					right
-					dark
+				<v-chip
+					v-if="file"
+					close
+					class="mb-8"
+					@click:close="removeFile()"
 				>
-					mdi-cloud-upload
-				</v-icon>
-			</v-btn>
+					{{ file.name }}
+					({{ fileSize }})
+				</v-chip>
+
+				<v-btn
+					v-if="file"
+					class="submit-button primary"
+					@click="submit()"
+				>
+					Submit
+					<v-icon
+						right
+						dark
+					>
+						mdi-cloud-upload
+					</v-icon>
+				</v-btn>
+			</div>
+			<v-progress-circular
+				v-else
+				:size="100"
+				:width="5"
+				indeterminate
+				color="primary"
+			>
+				waiting ...
+			</v-progress-circular>
 		</div>
 
-		<v-progress-circular
-			v-if="false"
-			:rotate="180"
-			:size="100"
-			:width="15"
-			:value="uploadPercentage"
-			color="pink"
-		>
-			{{ uploadPercentage }}
-		</v-progress-circular>
+
 	</div>
 </template>
 
 <script>
+  const VIDEO_TYPES = ['video/mp4', 'video/mpeg', 'video/quicktime'];
+
+  const toWav = require('audiobuffer-to-wav');
+
   export default {
     name: 'TFileInput',
 
     data: () => ({
       dragAndDropCapable: false,
       file: null,
-      uploadPercentage: 0,
+      duration: 0,
+      isPreload: false,
       isLoading: false,
       isDragover: false,
     }),
@@ -84,6 +99,7 @@
         return null;
       }
     },
+
     mounted() {
       this.dragAndDropCapable = this.determineDragAndDropCapable();
 
@@ -107,7 +123,11 @@
 
         this.$refs.fileForm.ondragover = () => {
           this.isDragover = true;
-        }
+        };
+
+        this.$refs.fileForm.ondragleave = () => {
+          this.isDragover = false;
+        };
       }
     },
 
@@ -119,11 +139,70 @@
             && 'FormData' in window
             && 'FileReader' in window;
       },
+
       triggerFileInput() {
         this.$refs.fileInput.$refs.input.click();
       },
+
       removeFile() {
         this.file = null;
+        this.isDragover = false;
+      },
+
+      submit() {
+        //TODO проверка на видео
+
+        if (VIDEO_TYPES.includes(this.file.type)) {
+          this.isPreload = true;
+          this.getAudioBuffer(this.file);
+
+        } else if (this.file.type === 'audio/wav') {
+          this.sendFile(this.file);
+        }
+      },
+
+      sendFile(file) {
+        console.log(file)
+        return 1;
+      },
+
+      getAudioBuffer(file) {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const reader = new FileReader();
+        let myBuffer;
+
+
+        const sampleRate = 16000;
+        const numberOfChannels = 1;
+
+        reader.onload = () => {
+          const videoFileAsBuffer = reader.result; // arraybuffer
+          audioContext.decodeAudioData(videoFileAsBuffer).then((decodedAudioData) => {
+
+            const duration = decodedAudioData.duration;
+
+            const offlineAudioContext = new OfflineAudioContext(numberOfChannels, sampleRate * duration, sampleRate);
+            const soundSource = offlineAudioContext.createBufferSource();
+
+            myBuffer = decodedAudioData;
+            soundSource.buffer = myBuffer;
+            soundSource.connect(offlineAudioContext.destination);
+            soundSource.start();
+
+            offlineAudioContext.startRendering().then((renderedBuffer) => {
+              this.isPreload = false;
+              const wav = toWav(renderedBuffer);
+              const wavFile = new File([wav], `${file.name}.wav`, {type: "audio/wav"} );
+              const objectUrl = URL.createObjectURL(wavFile);
+              console.log(wavFile)
+              window.open(objectUrl);
+            }).catch(function (err) {
+              console.log('Rendering failed: ' + err);
+            });
+          });
+        };
+
+        reader.readAsArrayBuffer(file); // video file
       }
     }
   }
@@ -132,7 +211,6 @@
 <style scoped>
 	.form {
 		display: flex;
-		height: 200px;
 		width: 100%;
 		margin: auto;
 		border-radius: 4px;
