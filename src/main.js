441fad667806758457551fcce9c43a1120b0d390
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-04-09 11:33:44 +0700

    fix routing

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueRecord from '@codekraft-studio/vue-record'
+import VueRouter from 'vue-router';
+import VueRecord from '@codekraft-studio/vue-record';
+import router from './router'
 
 Vue.use(VueRecord);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app');
