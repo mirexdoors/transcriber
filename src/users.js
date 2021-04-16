a9797beb947665b1bfa91507759c26a9ce2c2c12
Mikhail Prokudin <prokudin.mi@sibvaleo.com>
2021-04-16 18:59:10 +0700

    add new users

--- src/users.js
+++ src/users.js
@@ -13,5 +13,30 @@ export const users = [
     "id": 3,
     "login": "user2",
     "password": "Mnb[k4q"
-  }
+  },
+  {
+    "id": 4,
+    "login": "user4",
+    "password": "g0O5NNQ"
+  },
+  {
+    "id": 5,
+    "login": "user5",
+    "password": "expzWzE"
+  },
+  {
+    "id": 6,
+    "login": "user6",
+    "password": "1pBmO86"
+  },
+  {
+    "id": 7,
+    "login": "user7",
+    "password": "38F9kGp"
+  },
+  {
+    "id": 8,
+    "login": "user8",
+    "password": "oEVXT5Q"
+  },
 ]
